be19584c9645f5d9bef1c10c2382de2800191595
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-04 20:08:22 +0500

    o'rtachasini topish

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,9 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+from basic_function_parameter02 import calculate_sum
+def calculate_average(lst):
+    s=calculate_sum(lst)
+    ort=s/len(lst)
+    return ort
+print(calculate_average([1,2,3,4,5,6,7,8,9]))
